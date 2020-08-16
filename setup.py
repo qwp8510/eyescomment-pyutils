bfa83a69b2fd31cb8fe59b8a972cb459c068841d
weichen <a4793706@gmail.com>
2020-08-16 21:27:01 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='eyescomment-pyutils',
+    version='1.0.0',
+    description='weichen eyescomment project utils',
+    maintainer='weichen',
+    maintainer_email='a4703706@gmail.com',
+    url='https://github.com/qwp8510/eyescomment-pyutils',
+    packages=find_packages()
+)
