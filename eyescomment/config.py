fe57338172327b735e6530f7b0596dd3d4ae47df
weichen <a4793706@gmail.com>
2020-08-22 22:29:10 +0800

    update global config class

--- eyescomment/config.py
+++ eyescomment/config.py
@@ -2,17 +2,32 @@ import json
 
 
 class Config():
-    def __init__(self, Dir):
-        self.configDir = Dir
+    """ global config class"""
 
-    @property
-    def content(self):
-        with open(self.configDir) as js:
-            f = json.load(js)
-            js.close()
-        return f
+    _instance = None
 
-    def set_config(self, data):
-        with open(self.configDir, 'w') as js:
+    def __init__(self, config_dir=None):
+        self.config_dir = config_dir
+
+    @classmethod
+    def instance(cls):
+        if not cls._instance:
+            with open(cls._config_dir) as js:
+                cls._instance = json.load(js)
+                js.close()
+        return cls._instance
+
+    @classmethod
+    def set_dir(cls, config_dir):
+        cls._config_dir = config_dir
+
+    def dump(self, data):
+        with open(self.config_dir, 'w') as js:
             json.dump(data, js)
             js.close()
+
+    def read(self):
+        with open(self.config_dir, 'r') as js:
+            content = json.load(js)
+            js.close()
+        return content
