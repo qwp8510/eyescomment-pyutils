d62d9d936ec261eac1ebbc70ec0e0149781c725b
weichen <a4793706@gmail.com>
2020-08-22 18:17:28 +0800

    repush Regionapi

--- eyescomment/platform/api.py
+++ eyescomment/platform/api.py
@@ -1,3 +1,17 @@
-Unstaged changes after reset:
-M	eyescomment/platform/api.py
-D	eyescomment/youtube/channel_api.py
+import json
+from ..api import Api
+
+
+class RegionApi(Api):
+    def __init__(self, host, target_path, cache_path):
+        super(RegionApi, self).__init__(
+            host=host, target_path=target_path, cache_path=cache_path
+        )
+
+    def get(self, params=None):
+        data = super(RegionApi, self).get(
+            params={"filter": json.dumps(params)})
+        return data
+
+    def push(self, data):
+        super(RegionApi, self).post(json=data)
