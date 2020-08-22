ab769ab963b61d822292956d3cc80b8b4b678392
weichen <a4793706@gmail.com>
2020-08-22 18:13:40 +0800

    fix youtube folder api to group by db

---
+++ eyescomment/youtube/api.py
@@ -0,0 +1,39 @@
+from ..platform.api import RegionApi
+
+
+class YoutbeVideo(RegionApi):
+    """Videos of Youtube Channel from db"""
+
+    def __init__(self, host, cache_path, filter_params={}):
+        """
+        Args:
+            host (str): http url host.
+            path (str): http url path.
+            filter_params: filter params.
+        """
+        self.filter_params = filter_params
+        super(YoutbeVideo, self).__init__(
+            host=host, target_path='Youtube_videos', cache_path=cache_path)
+
+    def __getitem__(self, idx):
+        videos_detail = self.get(params=self.filter_params)
+        return videos_detail[idx]
+
+
+class Youtubechannel(RegionApi):
+    """Youtube Channel from db"""
+
+    def __init__(self, host, cache_path, filter_params={}):
+        """
+        Args:
+            host (str): http url host.
+            path (str): http url path.
+            filter_params: filter params.
+        """
+        self.filter_params = filter_params
+        super(Youtubechannel, self).__init__(
+            host=host, target_path='Youtube_channels', cache_path=cache_path)
+
+    def __getitem__(self, idx):
+        channels_detail = self.get(params=self.filter_params)
+        return channels_detail[idx]
