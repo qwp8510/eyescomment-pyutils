c01fa0a1f59937eaefc6a7dfd6db16ba978c498f
weichen <a4793706@gmail.com>
2020-09-20 12:48:56 +0800

    add function message

--- eyescomment/message_helper.py
+++ eyescomment/message_helper.py
@@ -1,17 +1,19 @@
 import sysv_ipc
 import logging
-
+import json
 
 logger = logging.getLogger(__name__)
 
 
 class MessageHelper():
     def __init__(self, queue_key=8020):
+        """ MessageHelper constructor"""
         self._queue_key = queue_key
         self._message_queue = None
 
     @property
     def message_queue(self):
+        """ Return system mq instance"""
         try:
             if not self._message_queue:
                 self._message_queue = sysv_ipc.MessageQueue(
@@ -24,16 +26,26 @@ class MessageHelper():
         return self._message_queue
 
     def publish(self, msg=None):
+        """ system mq publish message
+        Args:
+            msg(json(dict)): json type message
+
+        """
         if not self.message_queue:
             logger.error('publish message error due to message_queue is None')
             return
-        message = msg
+        message = json.dumps(msg)
         try:
             self.message_queue.send(message)
         except Exception as e:
             logger.error('publish ipc message to queue fail: {}'.format(e))
 
     def consume(self):
+        """ system mq consume
+        Returns:
+            sucess, message, priority
+
+        """
         try:
             (message, priority) = self.message_queue.receive()
             return True, message, priority
