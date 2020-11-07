f4d9bc3b134f192b5fce66e7a963414417cd8056
Daniel Chiang <44150378+qwp8510@users.noreply.github.com>
2020-11-07 18:00:55 +0800

    add test_api_get function in test_api

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,7 +1,7 @@
 import pytest
 from os import path
 
-from eyescomment.api import Login
+from eyescomment.api import Login, Api
 from eyescomment.config import Config
 
 CURRENT_PATH = path.dirname(path.abspath(__file__))
@@ -19,3 +19,26 @@ def test_get_login_access_token():
     valid_access_token(access_token)
     assert login.url == path.join(login.host, 'Users/login')
 
+
+def test_api_get():
+    TGOP_channel_data = {
+        "id": 1,
+        "channelName": "這群人TGOP",
+        "channelId": "UC6FcYHEm7SO1jpu5TKjNXEA",
+        "location": "TW",
+        "category": "Entertainment",
+        "language": "Chinese_Traditional",
+        "contact": "thisgroupofpeople@gmail.com",
+        "createdAt": "2008-06-07T00:00:00.000Z",
+        "updateLock": None,
+        "subscriber": None
+    }
+    Config.set_dir(path.join(CURRENT_PATH, 'config.json'))
+    api = Api(
+        host=Config.instance().get('PORTAL_SERVER'),
+        target_path='Youtube_channels',
+        cache_path=Config.instance().get('CACHE_DIR'))
+    filter_params = {'where': {'channelId': 'UC6FcYHEm7SO1jpu5TKjNXEA'}}
+    channel_data = api.get(params=filter_params)
+    assert isinstance(channel_data, list)
+    assert channel_data[0] == TGOP_channel_data
