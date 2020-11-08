6439e4a942ea9f317895c9289e354a0e5554899f
Daniel Chiang <44150378+qwp8510@users.noreply.github.com>
2020-11-08 17:04:10 +0800

    fix config to singleton instance

--- eyescomment/config.py
+++ eyescomment/config.py
@@ -1,33 +1,41 @@
-import json
+from __init__ import get_json_content
 
 
 class Config():
-    """ global config class"""
+    """ Global config class"""
 
     _instance = None
+    _config_dir = None
+    config = {}
 
-    def __init__(self, config_dir=None):
-        self.config_dir = config_dir
+    def __init__(self):
+        if self._config_dir:
+            self.config = get_json_content(self._config_dir)
 
     @classmethod
     def instance(cls):
+        """ Get singleton instance"""
         if not cls._instance:
-            with open(cls._config_dir) as js:
-                cls._instance = json.load(js)
-                js.close()
+            cls._instance = cls()
         return cls._instance
 
     @classmethod
     def set_dir(cls, config_dir):
+        """ Set config path
+        Args:
+            config_dir(str): config path
+
+        """
         cls._config_dir = config_dir
 
-    def dump(self, data):
-        with open(self.config_dir, 'w') as js:
-            json.dump(data, js)
-            js.close()
+    def get(self, key, fallback=None):
+        """ Get config value by key
+        Args:
+            key(str): dict key name
+            fallback: fall back value if key does not exist
 
-    def read(self):
-        with open(self.config_dir, 'r') as js:
-            content = json.load(js)
-            js.close()
-        return content
+        Returns:
+            dict value
+
+        """
+        return self.config.get(key, fallback)
